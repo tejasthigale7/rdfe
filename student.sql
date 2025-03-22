71bf55c3ffcde436275387595bbb413403f907f2
Tejas Thigale <tejasthigale7@gmail.com>
2025-03-22 13:12:37 +0530

    remove cooment

--- student.sql
+++ student.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 21, 2025 at 01:09 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +11,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `student`
+
 --
 
 -- --------------------------------------------------------
